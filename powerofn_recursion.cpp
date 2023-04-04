071deb11da7a4a3a7a796e7d4b53aa2f66b4c1cf
Kunal <kunalsmart46@gmail.com>
2023-04-04 21:55:19 +0530

    "power function using recursion"

---
+++ powerofn_recursion.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+
+//using recursion
+
+int power(int base,int pwr){
+    
+    if(pwr==0){
+        return 1;  //base case #1
+    }
+    if(pwr==1){
+        return base;  //base case #2
+    }
+
+    int ans=power(base,pwr/2); //recursive call;
+
+    if(pwr%2==0){   //even power
+        return ans*ans;
+    }else{
+        return base*ans*ans;  //odd power
+    }
+
+
+}
+int main(){
+
+    int base;
+    cout<<"Enter Base: "<<endl;
+    cin>>base;
+    
+    int pwr;
+    cout<<"Enter power: "<<endl;
+    cin>>pwr;
+
+    int ans=power(base,pwr);
+
+    cout<<"Answer: "<<ans<<endl;
+
+    return 0;
+
+}
