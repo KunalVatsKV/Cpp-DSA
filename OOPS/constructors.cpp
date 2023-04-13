5f2177bf23c0a660454731e39ad57a5b712e7aed
Kunal <kunalsmart46@gmail.com>
2023-04-13 11:36:36 +0530

    constructors

--- OOPS/constructors.cpp
+++ OOPS/constructors.cpp
@@ -5,13 +5,39 @@ using namespace std;
 
 class Hero{
 
+    public:
+
+    int health;
+
+
+    Hero(){
+        cout<<"Constructor called."<<endl;    //creating this constructor removes the default constructor
+    }
+
+    Hero(int health){ // Parameterised constructor
+
+        this->health=health;    //here "this" is a pointer which points to the object
+
+        cout<<"Address of this: "<<this<<endl;
+        
+    }
+
 
 
 };
 
 int main(){
 
-    Hero kunal;   
+    Hero kunal;   //this calls a constructor  //a defult constructor called kunal.Hero()
+
+
+    Hero vats(100);
+    cout<<"Address of the object 'vats': "<<&vats<<endl;
+    cout<<"heath of vats: "<<vats.health<<endl;
+
+
+
+    //object creation - constructor is invoked
 
   
 
