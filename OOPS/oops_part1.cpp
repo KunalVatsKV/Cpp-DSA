0bea5ac2196c67b2750b087aefe8e647f9333ccc
Kunal <kunalsmart46@gmail.com>
2023-04-13 11:00:13 +0530

    oops

--- OOPS/oops_part1.cpp
+++ OOPS/oops_part1.cpp
@@ -6,21 +6,35 @@ using namespace std;
 class Hero{
 
     //properties
-    public:
+    private:
     int health;
+    public:
     char level;
+
+    int getHealth(){
+        return health;              //this is a getter
+    }
+
+    void setHealth(int h){
+        health = h;                //this is a setter
+
+    }
 
 
 };
 
 int main(){
 
-    Hero kunal;
-    kunal.health = 100;
+    Hero kunal;   //statis allocation
+
+    Hero *ramesh = new Hero;   //dynamic allocation
+
+
     kunal.level = 'S';
+    kunal.setHealth(100); //we use a setter as the property is private.
 
-
-    cout<<"Health is: "<<kunal.health<<endl;   
+    //cout<<"Health is: "<<kunal.health<<endl;   this cannot work as the health property is private therfore we use a getter
+    cout<<"Health is: "<<kunal.getHealth()<<endl;
                                             //we can access the properties of a class using a . 
     cout<<"Level is: "<<kunal.level<<endl;
     
