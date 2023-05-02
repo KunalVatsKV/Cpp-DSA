a26a94b1186f88a7173c07fdeee17535430e1498
Kunal <kunalsmart46@gmail.com>
2023-05-03 02:01:00 +0530

    singly linked list

--- LinkedList/SinglyLinkedList.cpp
+++ LinkedList/SinglyLinkedList.cpp
@@ -22,13 +22,69 @@ class Node{
 
 };
 
+void InsertAtHead(Node* &head,int d){
+    Node* temp = new Node(d);
+    temp->next=head;
+    head=temp;
+}
+
+void InsertAtTail(Node* &tail,int d){
+    Node* temp = new Node(d);
+    tail->next=temp;
+    tail=temp;
+    
+}
+
+void print(Node* &head){
+    Node* temp=head;
+    
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+
+    cout<<endl;
+
+}
+
+
+
 
 int main(){
 
-    Node* nodel = new Node(20);
+    //created a new node
+    Node* node1 = new Node(20);
 
-    cout<<nodel->data<<endl;
-    cout<<nodel->next<<endl;
+    // cout<<node1->data<<endl;
+    // cout<<node1->next<<endl;
+
+    //head pointed to node1
+    Node* head=node1;
+
+    print(head);
+
+    InsertAtHead(head,12);
+
+    print(head);
+
+    InsertAtHead(head,3);
+
+    print(head);
+
+
+    //for tail
+ 
+    Node* tail=node1;
+
+    InsertAtTail(tail,30);
+    print(head);
+
+    InsertAtTail(tail,50);
+    print(head);
+
+
+
+
 
 
 
